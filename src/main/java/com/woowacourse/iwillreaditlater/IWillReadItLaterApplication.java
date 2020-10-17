1003e0fcdb0c76f6f0a75393cecef807453513c1
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-10-17 22:24:05 +0900

    chore: 프로젝트 구성

---
+++ src/main/java/com/woowacourse/iwillreaditlater/IWillReadItLaterApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.iwillreaditlater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IWillReadItLaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IWillReadItLaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woowacourse/iwillreaditlater/IWillReadItLaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.iwillreaditlater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IWillReadItLaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
