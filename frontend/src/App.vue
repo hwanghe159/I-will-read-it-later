1e2e7d1bf38177dc5090b96c47f1593bc20f1313
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-10-18 23:20:02 +0900

    feat: 글 등록 버튼, 글 등록 형식 틀 완성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,31 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
+  <v-app id="inspire">
+    <v-app id="inspire">
+      <navigation-drawer />
+      <v-main>
+        <v-container class="fill-height">
+          <v-row justify="center" align="center">
+            <v-col class="shrink"> </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+      <register-article-button />
+    </v-app>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import NavigationDrawer from "./components/NavigationDrawer";
+import RegisterArticleButton from "./components/RegisterArticleButton";
 
 export default {
   name: "App",
 
-  components: {
-    HelloWorld
-  },
+  components: { NavigationDrawer, RegisterArticleButton },
 
-  data: () => ({
-    //
-  })
+  data: () => ({}),
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
 };
 </script>
---
+++ frontend/src/components/RegisterArticleButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-btn bottom color="pink" dark fab fixed right @click="dialog = !dialog">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+
+    <v-dialog v-model="dialog" width="800px">
+      <register-article-form />
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import RegisterArticleForm from "./RegisterArticleForm";
+
+export default {
+  name: "RegisterArticleButton",
+
+  components: { RegisterArticleForm },
+  data: () => ({
+    dialog: false
+  })
+};
+</script>
+
+<style></style>
