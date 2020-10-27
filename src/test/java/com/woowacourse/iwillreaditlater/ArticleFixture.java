d2b48cf01f01d5024c129b10ed011cf4883e03c0
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-10-27 16:20:00 +0900

    feat: 게시글 전체조회 구현

---
+++ src/test/java/com/woowacourse/iwillreaditlater/ArticleFixture.java
@@ -0,0 +1,13 @@
+package com.woowacourse.iwillreaditlater;
+
+public class ArticleFixture {
+
+    public static final Long ARTICLE_ID_1 = 1L;
+    public static final Long ARTICLE_ID_2 = 2L;
+    public static final String ARTICLE_URL_1 = "URL1";
+    public static final String ARTICLE_URL_2 = "URL2";
+    public static final String ARTICLE_TITLE_1 = "제목1";
+    public static final String ARTICLE_TITLE_2 = "제목2";
+    public static final String ARTICLE_CONTENT_1 = "내용1";
+    public static final String ARTICLE_CONTENT_2 = "내용2";
+}
--- src/test/java/com/woowacourse/iwillreaditlater/service/ArticleServiceTest.java
+++ src/test/java/com/woowacourse/iwillreaditlater/service/ArticleServiceTest.java
@@ -3,6 +3,7 @@ package com.woowacourse.iwillreaditlater.service;
 import com.woowacourse.iwillreaditlater.domain.Article;
 import com.woowacourse.iwillreaditlater.domain.ArticleRepository;
 import com.woowacourse.iwillreaditlater.dto.ArticleCreateRequest;
+import com.woowacourse.iwillreaditlater.dto.ArticleResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static com.woowacourse.iwillreaditlater.ArticleFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -22,19 +28,39 @@ class ArticleServiceTest {
     @Mock
     private ArticleRepository articleRepository;
 
+    private Article article1;
+
+    private Article article2;
+
     @BeforeEach
     void setUp() {
         articleService = new ArticleService(articleRepository);
+        article1 = new Article(ARTICLE_ID_1, ARTICLE_URL_1, ARTICLE_TITLE_1, ARTICLE_CONTENT_1);
+        article2 = new Article(ARTICLE_ID_2, ARTICLE_URL_2, ARTICLE_TITLE_2, ARTICLE_CONTENT_2);
+    }
+
+    @DisplayName("존재하는 모든 게시글을 반환할 수 있어야 한다.")
+    @Test
+    void getArticlesTest() {
+        given(articleRepository.findAll()).willReturn(Arrays.asList(article1, article2));
+
+        List<ArticleResponse> articleResponses = articleService.getArticles();
+
+        assertAll(
+            () -> assertThat(articleResponses).hasSize(2),
+            () -> assertThat(articleResponses.get(0).getId()).isEqualTo(ARTICLE_ID_1),
+            () -> assertThat(articleResponses.get(1).getId()).isEqualTo(ARTICLE_ID_2)
+        );
     }
 
     @DisplayName("게시글을 저장하고 저장된 게시글의 id를 반환할 수 있어야 한다.")
     @Test
     void addArticleTest() {
-        given(articleRepository.save(any())).willReturn(new Article(1L, "", "", ""));
+        given(articleRepository.save(any())).willReturn(article1);
 
-        ArticleCreateRequest request = new ArticleCreateRequest("", "", "");
+        ArticleCreateRequest request = new ArticleCreateRequest(ARTICLE_URL_1, ARTICLE_TITLE_1, ARTICLE_CONTENT_1);
         Long articleId = articleService.addArticle(request);
 
-        assertThat(articleId).isEqualTo(1L);
+        assertThat(articleId).isEqualTo(ARTICLE_ID_1);
     }
 }
