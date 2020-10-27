9a766bea6e52eda7b740f54b6cdac2c9bceed383
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-10-27 17:56:14 +0900

    feat: URL관련 예외를 MalformedURLException로 통일

--- src/test/java/com/woowacourse/iwillreaditlater/service/MetadataServiceTest.java
+++ src/test/java/com/woowacourse/iwillreaditlater/service/MetadataServiceTest.java
@@ -1,12 +1,13 @@
 package com.woowacourse.iwillreaditlater.service;
 
 import com.woowacourse.iwillreaditlater.dto.MetadataResponse;
-import org.jsoup.HttpStatusException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
-import java.net.UnknownHostException;
+import java.net.MalformedURLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -27,17 +28,11 @@ class MetadataServiceTest {
         );
     }
 
-    @DisplayName("존재하지 않는 url이 입력되면 예외가 발생되어야 한다.")
-    @Test
-    void getMetadataFailByNotExistUrlTest() {
-        assertThatThrownBy(() -> metadataService.getMetadata("https://존재하지않는url.com/2"))
-            .isInstanceOf(UnknownHostException.class);
-    }
-
-    @DisplayName("존재하지만 올바르지 않은 url이 입력되면 예외가 발생되어야 한다.")
-    @Test
-    void getMetadataFailByInvalidUrlTest() {
-        assertThatThrownBy(() -> metadataService.getMetadata("https://jjjjunodiary.tistory.com/111"))
-            .isInstanceOf(HttpStatusException.class);
+    @DisplayName("올바르지 않은 url이 입력되면 예외가 발생되어야 한다.")
+    @ValueSource(strings = {"https://존재하지않는url.com/2", "https://jjjjunodiary.tistory.com/111"})
+    @ParameterizedTest
+    void getMetadataFailByInvalidUrlTest(String input) {
+        assertThatThrownBy(() -> metadataService.getMetadata(input))
+            .isInstanceOf(MalformedURLException.class);
     }
 }
