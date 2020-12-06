370237d93e6f18881bd255426abe4770b4f437ac
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-12-06 23:35:10 +0900

    refactor: 테스트 간 격리를 위한 truncate.sql 추가

--- src/test/java/com/woowacourse/iwillreaditlater/controller/ControllerTest.java
+++ src/test/java/com/woowacourse/iwillreaditlater/controller/ControllerTest.java
@@ -2,12 +2,12 @@ package com.woowacourse.iwillreaditlater.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
 
+@Sql("/truncate.sql")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class ControllerTest {
 
     protected MockMvc mockMvc;
